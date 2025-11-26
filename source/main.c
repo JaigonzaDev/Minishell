8de500fb31cb0cbd14a26dae97f7f39a02dbdba8
Carlos <carlos@DESKTOP-17KA2TH.localdomain>
2025-11-26 16:08:25 +0100

    main_fix_env

--- source/main.c
+++ source/main.c
@@ -15,28 +15,28 @@
 
 char *read_line(int type)
 {
-    char *buf = NULL;
-    (void)type;
+	char *buf = NULL;
+	(void)type;
 
-    while (1)
-    {
-        buf = readline(prompt(E_PROMPT_MAIN));
-        if (buf == NULL)
-        {
-            if (feof(stdin))
-                exit(EXIT_SUCCESS);
-            else if (errno == EINTR)
-            {
-                errno = 0;
-            }
-            else
-                exit(EXIT_FAILURE);
-        }
-        break;
-    }
-    if (buf[0] != '\0')
-        add_history(buf);
-    return (buf);
+	while (1)
+	{
+		buf = readline(prompt(E_PROMPT_MAIN));
+		if (buf == NULL)
+		{
+			if (feof(stdin))
+				exit(EXIT_SUCCESS);
+			else if (errno == EINTR)
+			{
+				errno = 0;
+			}
+			else
+				exit(EXIT_FAILURE);
+		}
+		break;
+	}
+	if (buf[0] != '\0')
+		add_history(buf);
+	return (buf);
 }
 
 int bash_execute(t_token *tokens, t_env *env)
@@ -49,67 +49,70 @@ int bash_execute(t_token *tokens, t_env *env)
 	// Ejecutar usando el sistema de pipes
 	status = execute_pipeline(tokens, env);
 	
-    return (status);
+	return (status);
 	// // Actualizar status en variable global para $?
 	// update_exit_status(status);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-    char *line;
-    t_token *tokens;
-    t_env *env;
-    int status;
-    // pid_t pid;
-    (void)argc;
-    (void)argv;
+	char *line;
+	t_token *tokens;
+	t_env *env;
+	int status;
+	// pid_t pid;
+	(void)argc;
+	(void)argv;
 
 
-    env = NULL;
-    tokens = NULL;
-    line = NULL;
+	env = NULL;
+	tokens = NULL;
+	line = NULL;
 	main_signal_config();
-    while ((line = read_line(E_PROMPT_MAIN)))
-    {
-        env_save(envp, &env);
-        // Si la línea empieza por '#', se ignora
-        if (line[0] == '#')
-        {
-            free(line);
-            // continue;
-        }
-        // // // Crear un nuevo proceso para manejar la línea
-        // pid = fork();
-        // if (pid < 0)
-        // {
-        //     perror("fork");
-        //     // continue;
-        // }
-        // else if (pid == 0)
-        // {
-    	// 	child_signal_config();
-            // Separar operadores pegados al texto
+	// CARLOS: >Mover env_save antes bucle para que las variables de export persistan<
+	env_save(envp, &env);
+	while ((line = read_line(E_PROMPT_MAIN)))
+	{
+		// Si la línea empieza por '#', se ignora
+		if (line[0] == '#')
+		{
+			free(line);
+			continue;
+		}
+		// // // Crear un nuevo proceso para manejar la línea
+		// pid = fork();
+		// if (pid < 0)
+		// {
+		//     perror("fork");
+		//     // continue;
+		// }
+		// else if (pid == 0)
+		// {
+		// 	child_signal_config();
+			// Separar operadores pegados al texto
 
-            char *separated_line = separate_operators(line);
-            if (separated_line)
-            {
-                free(line);
-                line = separated_line;
-            }
-            tokens = bash_split(&line, env);
-            if ((status = parse_commands_new(&tokens)) == 0)
-            {
-                debug_parsing(tokens);
-                status = bash_execute(tokens, env);
-            }
-            exit (status);
-        // }
-        // else
-        // {
-            // waitpid(pid, &status, 0); // Esperar al hijo
-            // update_exit_status(WEXITSTATUS(status)); // Actualizar el estado de salida{
-        // }
-        env_freeall(&env);
-    }
-    return (EXIT_SUCCESS);
+			char *separated_line = separate_operators(line);
+			if (separated_line)
+			{
+				free(line);
+				line = separated_line;
+			}
+			tokens = bash_split(&line, env);
+			if ((status = parse_commands_new(&tokens)) == 0)
+			{
+				debug_parsing(tokens);
+				status = bash_execute(tokens, env);
+			}
+			//exit (status);
+		// }
+		// else
+		// {
+			// waitpid(pid, &status, 0); // Esperar al hijo
+			// update_exit_status(WEXITSTATUS(status)); // Actualizar el estado de salida{
+		// }
+		// CARLOS: >QUITAR env_freeall de aquí para no liberar en cada iteración<
+	}
+	// CARLOS: >Mover env_freeall DESPUÉS del bucle para liberar al final del programa<
+	env_freeall(&env);
+	return (EXIT_SUCCESS);
 }
